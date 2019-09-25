2ee668f003c0ee40fd22c36cf41ef94937133129
sophieannebourgine <sophieannebourgine@gmail.com>
2019-09-25 12:47:49 +0200

    bg image

--- client/src/components/pages/Home.jsx
+++ client/src/components/pages/Home.jsx
@@ -8,6 +8,40 @@ export default function Home() {
         src="./background-image.png"
         alt="backgroundimage"
       />
+      {/* <div id="fullpage">
+        <div class="section" id="section1">
+          <div class="section-content">
+            <h1>
+              <i class="fa fa-check-circle-o"></i>
+            </h1>
+            <h1>Welcome to My SITE</h1>
+          </div>
+        </div>
+        <div class="section" id="section2">
+          section2
+        </div>
+        <div class="section" id="section3">
+          <div class="slide" id="slide1">
+            {' '}
+            Slide 1{' '}
+          </div>
+          <div class="slide" id="slide2">
+            {' '}
+            Slide 2{' '}
+          </div>
+          <div class="slide" id="slide3">
+            {' '}
+            Slide 3{' '}
+          </div>
+          <div class="slide" id="slide4">
+            {' '}
+            Slide 4{' '}
+          </div>
+        </div>
+        <div class="section" id="section4">
+          section4
+        </div>
+      </div> */}
     </div>
   )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -11,4 +11,5 @@ ReactDOM.render(
   </Router>,
   document.getElementById('root')
 )
+
 // registerServiceWorker();
