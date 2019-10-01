3f5a4c75149d4174f676a606e25b6326d7567294
Aida <Aida@MacBook-Pro-de-Aida.local>
2019-10-01 18:12:06 +0200

    add new property

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,14 +7,14 @@ import ForRent from './pages/ForRent'
 import ContactUs from './pages/ContactUs'
 import Favorites from './pages/Favorites'
 import Countries from './pages/Countries'
-// import AddCountry from './pages/AddCountry'
 import Secret from './pages/Secret'
 import Login from './pages/Login'
 import Signup from './pages/Signup'
-// import Test from './pages/Test'
 import AboutUs from './pages/AboutUs'
 import OneProperty from './pages/OneProperty'
 import Map from './pages/Map'
+import AddNewProperty from './pages/AddNewProperty'
+import EditProperty from './pages/Edit'
 
 export default function App() {
   return (
@@ -25,13 +25,13 @@ export default function App() {
         <Route path="/forsale" component={ForSale} />
         <Route path="/forrent" component={ForRent} />
         <Route path="/favorites" component={Favorites} />
-        {/* <Route path="/test" component={Test} /> */}
         <Route path="/aboutus" component={AboutUs} />
         <Route path="/detail/:id" component={OneProperty} />
         <Route path="/contactus" component={ContactUs} />
         <Route path="/countries" component={Countries} />
-        {/* <Route path="/add-country" component={AddCountry} /> */}
         <Route path="/signup" component={Signup} />
+        <Route path="/add-new-property" component={AddNewProperty} />
+        <Route path="/manage-property" component={EditProperty} />
         <Route path="/login" component={Login} />
         <Route path="/secret" component={Secret} />
         <Route path="/map" component={Map} />
--- client/src/components/pages/AddNewProperty.jsx
+++ client/src/components/pages/AddNewProperty.jsx
@@ -13,6 +13,13 @@ export default function AddProperty(props) {
     rooms: '',
     bedrooms: '',
     others: '',
+    isCheckedGarden: false,
+    isCheckedFireplace: false,
+    isCheckedCaretaker: false,
+    isCheckedBalcony: false,
+    isCheckedPool: false,
+    isCheckedTerrace: false,
+    isCheckedParking: false,
     description: '',
     pictures: '',
   })
@@ -25,6 +32,10 @@ export default function AddProperty(props) {
       ...state,
       [event.target.name]: event.target.value,
     })
+  }
+  function handleChange(e) {
+    let value = e.target.type === 'checkbox' ? e.target.checked : e.target.value
+    setState({ ...state, [e.target.name]: value })
   }
 
   function handleClick(e) {
@@ -45,16 +56,24 @@ export default function AddProperty(props) {
       pictures: state.pictures,
     }
     api
-      .addCountry(data)
+      .addProperty(data)
       .then(result => {
         console.log('SUCCESS!')
         setState({
-          name: '',
-          capitals: '',
-          area: '',
+          title: '',
+          type: '',
+          king: '',
+          location: '',
+          localisation: '',
+          budget: '',
+          size: '',
+          rooms: '',
+          bedrooms: '',
+          others: '',
           description: '',
+          pictures: '',
         })
-        setMessage(`Your country '${state.name}' has been created`)
+        setMessage(`The new property '${state.title}' has been created`)
         setTimeout(() => {
           setMessage(null)
         }, 2000)
@@ -62,45 +81,285 @@ export default function AddProperty(props) {
       .catch(err => setState({ message: err.toString() }))
   }
   return (
-    <div className="AddCountry">
-      <h2>Add country</h2>
-      <form>
-        Name:{' '}
-        <input
-          type="text"
-          value={state.name}
-          name="name"
-          onChange={handleInputChange}
-        />{' '}
-        <br />
-        Capitals:{' '}
-        <input
-          type="text"
-          value={state.capitals}
-          name="capitals"
-          onChange={handleInputChange}
-        />{' '}
-        <br />
-        Area:{' '}
-        <input
-          type="number"
-          value={state.area}
-          name="area"
-          onChange={handleInputChange}
-        />{' '}
-        <br />
-        Description:{' '}
-        <textarea
-          value={state.description}
-          name="description"
-          cols="30"
-          rows="10"
-          onChange={handleInputChange}
-        />{' '}
-        <br />
-        <button onClick={e => handleClick(e)}>Create country</button>
-      </form>
-      {message && <div className="info">{message}</div>}
+    <div className="addProperty">
+      <div className="">
+        <div className="">
+          <div className="">
+            <h2 className="contact-title">Add New Property</h2>
+            <form
+              onSubmit={handleClick}
+              onChange={handleInputChange}
+              enctype="multipart/form-data"
+            >
+              <div className="form-group col-md-6">
+                {/* <label for="InputTitle">Title</label> */}
+
+                <input
+                  type="text"
+                  name="title"
+                  value={state.title}
+                  className="form-control"
+                  placeholder="Title"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                {/* <label for="InputType">Type</label> */}
+                <input
+                  type="text"
+                  name="type"
+                  value={state.type}
+                  className="form-control"
+                  placeholder="Type : Apartment ? Houseboat ? Loft ? Duplex ? Triplex ? Penthouse ? "
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                {/* <label for="InputKind">For rent/for sale</label> */}
+                <input
+                  type="text"
+                  name="kind"
+                  value={state.kind}
+                  className="form-control"
+                  placeholder="For Rent ? For Sale ?"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                {/* <label for="InputLocation">Location</label> */}
+                <input
+                  type="text"
+                  name="location"
+                  value={state.location}
+                  className="form-control"
+                  placeholder="Location : Paris 1, Paris 2, Paris 3, ..."
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group col-md-6">
+                {/* <label for="InputLocalisation">Localisation</label> */}
+                <br />
+                <input
+                  type="text"
+                  name="localisation"
+                  value={state.localisation}
+                  className="form-control"
+                  placeholder="Coordinates: [48.8558962, 2.3577906]"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <br />
+              <br />
+
+              <div className="form-group col-md-3">
+                {/* <label for="InputBudget">Price</label> */}
+                <input
+                  type="number"
+                  name="budget"
+                  value={state.budget}
+                  className="form-control"
+                  placeholder="Price in €"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div className="form-group col-md-3">
+                {/* <label for="InputSize">
+                  Size in m<sup>2</sup>
+                </label> */}
+                <input
+                  type="number"
+                  name="size"
+                  value={state.size}
+                  className="form-control"
+                  placeholder="Size in m"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div class="form-group col-md-3">
+                {/* <label for="InputRooms">Number of rooms</label> */}
+                <input
+                  id="InputRooms"
+                  name="rooms"
+                  value={state.rooms}
+                  type="number"
+                  className="form-control"
+                  placeholder="Number of rooms"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div class="form-group col-md-3">
+                {/* <label for="InputBedRooms">Number of Bedrooms</label> */}
+                <input
+                  id="InputBedRooms"
+                  name="bedrooms"
+                  value={state.bedrooms}
+                  type="number"
+                  className="form-control"
+                  placeholder="Number of Bedrooms"
+                  onChange={handleInputChange}
+                />
+              </div>
+              <div class="form-group">
+                <label for="InputOthers">Other criterias ? </label>
+                <div className="containerList">
+                  <div className="itemList active">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      name="isCheckedGarden"
+                      id="garden"
+                      value={state.others}
+                      onChange={handleChange}
+                      checked={state.isCheckedGarden}
+                    ></input>
+                    <label htmlFor="garden">Garden</label>
+                  </div>
+                  <div className="itemList active">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      name="isCheckedFireplace"
+                      id="fireplace"
+                      value={state.others}
+                      onChange={handleChange}
+                      checked={state.isCheckedFireplace}
+                    ></input>
+                    <label htmlFor="fireplace">Fireplace</label>
+                  </div>
+                  <div className="itemList active">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      name="isCheckedCaretaker"
+                      id="guardian"
+                      value={state.others}
+                      onChange={handleChange}
+                      checked={state.isCheckedCaretaker}
+                    ></input>
+                    <label htmlFor="guardian">Caretaker</label>
+                  </div>
+                  <div className="itemList active">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      name="isCheckedBalcony"
+                      id="balcony"
+                      value={state.others}
+                      onChange={handleChange}
+                      checked={state.isCheckedBalcony}
+                    ></input>
+                    <label htmlFor="balcony">Balcony</label>
+                  </div>
+                  <div className="itemList active">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      name="isCheckedPool"
+                      id="pool"
+                      value={state.others}
+                      onChange={handleChange}
+                      checked={state.isCheckedPool}
+                    ></input>
+                    <label htmlFor="Swimming-Pool">Swimming Pool</label>
+                  </div>
+
+                  <div className="itemList active">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      name="isCheckedTerrace"
+                      id="terrace"
+                      value={state.others}
+                      onChange={handleChange}
+                      checked={state.isCheckedTerrace}
+                    ></input>
+                    <label htmlFor="terrace">Terrace</label>
+                  </div>
+                  <div className="itemList active">
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      name="isCheckedParking"
+                      id="parking"
+                      value={state.others}
+                      onChange={handleChange}
+                      checked={state.isCheckedParking}
+                    ></input>
+                    <label htmlFor="parking">Parking</label>
+                  </div>
+                </div>
+              </div>
+              <br />
+              <div className="form-group col-md-12">
+                {/* <label for="description">Description</label> */}
+                <textarea
+                  class="form-control"
+                  id="textarea"
+                  rows="5"
+                  columns="7"
+                  placeholder="Add a little description of the property"
+                ></textarea>
+              </div>
+              <div className="form-group col-md-6">
+                <label for="pictures">Pictures</label>
+                <input id="property-imgs" type="file" name="image" />
+              </div>
+              <div class="button-submit-contact">
+                <button
+                  type="submit"
+                  class="btn btn-primary"
+                  onClick={e => handleClick(e)}
+                >
+                  Add
+                </button>
+              </div>
+            </form>
+            <br />
+
+            <div className="background-image-signup"></div>
+          </div>
+        </div>
+      </div>
     </div>
+    // <div className="AddProperty">
+    //   <h2>Add new Property</h2>
+    //   <form>
+    //   Title:
+    //     <input
+    //       type="text"
+    //       value={state.name}
+    //       name="name"
+    //       onChange={handleInputChange}
+    //     />{' '}
+    //     <br />
+    //     Capitals:{' '}
+    //     <input
+    //       type="text"
+    //       value={state.capitals}
+    //       name="capitals"
+    //       onChange={handleInputChange}
+    //     />{' '}
+    //     <br />
+    //     Area:{' '}
+    //     <input
+    //       type="number"
+    //       value={state.area}
+    //       name="area"
+    //       onChange={handleInputChange}
+    //     />{' '}
+    //     <br />
+    //     Description:{' '}
+    //     <textarea
+    //       value={state.description}
+    //       name="description"
+    //       cols="30"
+    //       rows="10"
+    //       onChange={handleInputChange}
+    //     />{' '}
+    //     <br />
+    //     <button onClick={e => handleClick(e)}>Create country</button>
+    //   </form>
+    //   {message && <div className="info">{message}</div>}
+    // </div>
   )
 }
